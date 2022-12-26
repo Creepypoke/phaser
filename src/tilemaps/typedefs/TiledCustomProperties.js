92a5148d878fd62d4ca783017ad8f4317e0e48e2
Anton Yessaulenko <creepypoke@gmail.com>
2022-12-26 18:36:46 +0300

    Added custom properties type of Tiled editor

---
+++ src/tilemaps/typedefs/TiledCustomProperties.js
@@ -0,0 +1,73 @@
+/**
+ * @typedef {object} TiledCustomPropertyBool
+ * @since 3.60.0
+ *
+ * @property {"bool"} type
+ * @property {boolean} value
+ * @property {string} name
+ * @property {string} [propertytype]
+ */
+
+/**
+ * @typedef {object} TiledCustomPropertyColor
+ * @since 3.60.0
+ *
+ * @property {"color"} type
+ * @property {string} value
+ * @property {string} name
+ * @property {string} [propertytype]
+ */
+
+/**
+ * @typedef {object} TiledCustomPropertyFile
+ * @since 3.60.0
+ *
+ * @property {"file"} type
+ * @property {string} value
+ * @property {string} name
+ * @property {string} [propertytype]
+ */
+
+/**
+ * @typedef {object} TiledCustomPropertyFloat
+ * @since 3.60.0
+ *
+ * @property {"float"} type
+ * @property {string} number
+ * @property {string} name
+ * @property {string} [propertytype]
+ */
+
+/**
+ * @typedef {object} TiledCustomPropertyInt
+ * @since 3.60.0
+ *
+ * @property {"int"} type
+ * @property {string} number
+ * @property {string} name
+ * @property {string} [propertytype]
+ */
+
+/**
+ * @typedef {object} TiledCustomPropertyObject
+ * @since 3.60.0
+ *
+ * @property {"object"} type
+ * @property {string} number
+ * @property {string} name
+ * @property {string} [propertytype]
+ */
+
+/**
+ * @typedef {object} TiledCustomPropertyString
+ * @since 3.60.0
+ *
+ * @property {"string"} type
+ * @property {string} string
+ * @property {string} name
+ * @property {string} [propertytype]
+ */
+
+/**
+ * @typedef {TiledCustomPropertyBool|TiledCustomPropertyColor|TiledCustomPropertyFile|TiledCustomPropertyFloat|TiledCustomPropertyInt|TiledCustomPropertyObject|TiledCustomPropertyString} TiledCustomProperty - Tiled Map properties declaration
+ */
